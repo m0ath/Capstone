3caa7177682024f852a48570b2a1baf833810558
m.alghanmy-t <m.alghanmy-t@tamkeentech.sa>
2020-12-07 11:31:41 +0300

    c4

--- auth.py
+++ auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'm0ath.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'Capstone'
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
 # AuthError Exception
 '''
